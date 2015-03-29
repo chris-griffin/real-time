d999fe70c3cd2436c1dff008b99d67296e8ba6dc
cwg23 <cwg23@georgetown.edu>
2015-03-29 15:30:31 -0400

    refactoring

--- importdata.py
+++ importdata.py
@@ -1,37 +1,50 @@
+from PIL import Image, ImageFont, ImageDraw
 from google.transit import gtfs_realtime_pb2
 import nyct_subway_pb2
 import urllib
 import datetime
+from time import sleep
 import math
 import os
-import config
+from config import *
 
 times = []
 out = ''
 	
 mtafeed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + mtakey + '&feed_id=1')
+response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + MTA_KEY + '&feed_id=1')
 mtafeed.ParseFromString(response.read())
 current_time = datetime.datetime.now()
-for entity in mtafeed.entity:
-                if entity.trip_update:
-                    for update in entity.trip_update.stop_time_update:
-                        if update.stop_id == stopid:
-				time = update.arrival.time
-				if time == 0:
-					time = update.departure.time
-				time = datetime.datetime.fromtimestamp(time)
-				time = math.trunc(((time - current_time).total_seconds()) / 60)
-				times.append(time)
-times.sort()
-for time in times[:num_trains]:
-	out+=str(time)
-	if num_trains > 1:
-		out+=str(', ')
-		num_trains = num_trains - 1
-print out
+for stop in STOP_IDS:
+	for entity in mtafeed.entity:
+	                if entity.trip_update:
+	                    for update in entity.trip_update.stop_time_update:
+	                        if update.stop_id == stop:
+					time = update.arrival.time
+					if time == 0:
+						time = update.departure.time
+					time = datetime.datetime.fromtimestamp(time)
+					time = math.trunc(((time - current_time).total_seconds()) / 60)
+					times.append(time)
+	times.sort()
+	for time in times[:NUM_TRAINS]:
+		out+=str(time)
+		temp_num_trains = NUM_TRAINS
+		if temp_num_trains > 1:
+			out+=str(',')
+			temp_num_trains = temp_num_trains - 1
+	
+	staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
+	draw = ImageDraw.Draw(staticimg)
+	font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSans.ttf', 12)
+	draw.text((14, 1), out,(75,75,75),font=font)
+	staticimg.save('dynamicimages/dynamictime.ppm')
+	times = []
+	out = ''
 
+	os.system('sudo ./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -j -D 1 -m 5000 rpi-rgb-led-matrix/dynamictime.ppm')
 
+	
 
 
 
