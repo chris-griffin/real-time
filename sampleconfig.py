4368ae88214b416f87f16cd62b7fb6c0fe763f4b
cwg23 <cwg23@georgetown.edu>
2015-03-29 16:44:07 -0400

    adding a sample config file

---
+++ sampleconfig.py
@@ -0,0 +1,5 @@
+#Change this file name to config.py and update appropriately
+
+MTA_KEY = 'your mta developer key' #obtain one at http://web.mta.info/developers/developer-data-terms.html
+NUM_TRAINS = 3  #the number of trains to display for each station/direction combination  
+STOP_IDS = ['230N', '419N', '230S']  #an array of stations/directions that you would like displayed. Find these in the stations file in staticdata
