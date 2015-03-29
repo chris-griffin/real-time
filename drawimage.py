063a1d33a2a6d3ea33a86c70abcced7f0edc05f2
cwg23 <cwg23@georgetown.edu>
2015-03-29 16:14:16 -0400

    removing files that are no longer needed

--- drawimage.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageFont, ImageDraw
-
-staticimg = Image.open('staticimages/23N.ppm')
-draw = ImageDraw.Draw(staticimg)
-font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSans.ttf', 12)
-draw.text((14, 1),'3,5,18,24',(50,50,50),font=font)
-staticimg.save('dynamicimages/23N.ppm')
-
