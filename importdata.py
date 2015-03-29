4965431a1234a07eb90b340f789ac181a92edf1c
cwg23 <cwg23@georgetown.edu>
2015-03-29 16:10:22 -0400

    fixing some bugs

--- importdata.py
+++ importdata.py
@@ -10,39 +10,43 @@ from config import *
 
 times = []
 out = ''
-	
-mtafeed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + MTA_KEY + '&feed_id=1')
-mtafeed.ParseFromString(response.read())
-current_time = datetime.datetime.now()
-for stop in STOP_IDS:
-	for entity in mtafeed.entity:
-	                if entity.trip_update:
-	                    for update in entity.trip_update.stop_time_update:
-	                        if update.stop_id == stop:
-					time = update.arrival.time
-					if time == 0:
-						time = update.departure.time
-					time = datetime.datetime.fromtimestamp(time)
-					time = math.trunc(((time - current_time).total_seconds()) / 60)
-					times.append(time)
-	times.sort()
-	for time in times[:NUM_TRAINS]:
-		out+=str(time)
-		temp_num_trains = NUM_TRAINS
-		if temp_num_trains > 1:
-			out+=str(',')
-			temp_num_trains = temp_num_trains - 1
-	
-	staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
-	draw = ImageDraw.Draw(staticimg)
-	font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSans.ttf', 12)
-	draw.text((14, 1), out,(75,75,75),font=font)
-	staticimg.save('dynamicimages/dynamictime.ppm')
-	times = []
-	out = ''
 
-	os.system('sudo ./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -j -D 1 -m 5000 rpi-rgb-led-matrix/dynamictime.ppm')
+while True:
+	try:
+		mtafeed = gtfs_realtime_pb2.FeedMessage()
+		response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=' + MTA_KEY + '&feed_id=1')
+		mtafeed.ParseFromString(response.read())
+		current_time = datetime.datetime.now()
+		for stop in STOP_IDS:
+			for entity in mtafeed.entity:
+			                if entity.trip_update:
+			                    for update in entity.trip_update.stop_time_update:
+			                        if update.stop_id == stop:
+							time = update.arrival.time
+							if time == 0:
+								time = update.departure.time
+							time = datetime.datetime.fromtimestamp(time)
+							time = math.trunc(((time - current_time).total_seconds()) / 60)
+							times.append(time)
+			times.sort()
+			for time in times[:NUM_TRAINS]:
+				out+=str(time)
+				out+=str(',')
+			out = out[:-1]
+			
+			staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
+			draw = ImageDraw.Draw(staticimg)
+			font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSans.ttf', 12)
+			draw.text((14, 1), out,(100,100,100),font=font)
+			staticimg.save('dynamicimages/dynamictime.ppm')
+			times = []
+			out = ''
+		
+			os.system('sudo ./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -j -D 1 -m 5000 dynamicimages/dynamictime.ppm')
+	except Exception:
+		pass
+
+
 
 	
 
