8844d2f72ee4c9d44f174e73e4eccfe856019ba3
cwg23 <cwg23@georgetown.edu>
2015-07-28 23:11:16 -0400

    updating images and small improvements

--- importdata.py
+++ importdata.py
@@ -7,6 +7,7 @@ from time import sleep
 import math
 import os
 from config import *
+import traceback
 
 times = []
 out = ''
@@ -23,12 +24,15 @@ while True:
 			                    for update in entity.trip_update.stop_time_update:
 			                        if update.stop_id == stop:
 							time = update.arrival.time
-							if time == 0:
+							if time <= 0:
 								time = update.departure.time
 							time = datetime.datetime.fromtimestamp(time)
 							time = math.trunc(((time - current_time).total_seconds()) / 60)
 							times.append(time)
 			times.sort()
+			for time in times:
+				if time < 0:
+					times.remove(time)
 			for time in times[:NUM_TRAINS]:
 				out+=str(time)
 				out+=str(',')
@@ -37,14 +41,14 @@ while True:
 			staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
 			draw = ImageDraw.Draw(staticimg)
 			font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSans.ttf', 12)
-			draw.text((14, 1), out,(100,100,100),font=font)
+			draw.text((16, 1), out,(200,200,200),font=font)
 			staticimg.save('dynamicimages/dynamictime.ppm')
 			times = []
 			out = ''
 		
-			os.system('sudo ./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -j -D 1 -m 5000 dynamicimages/dynamictime.ppm')
+			os.system('sudo ./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -b 50 -D 1 -m 5000 dynamicimages/dynamictime.ppm')
 	except Exception:
-		pass
+		print traceback.format_exc()
 
 
 
